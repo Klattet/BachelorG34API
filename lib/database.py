0f93f060795c0adda8f4681b093051ec58f4f0ba
A-Lexion <lyngvollan@protonmail.com>
2024-02-10 14:54:24 +0100

    Can now search for relevant documents based for a text on keywords.

--- lib/database.py
+++ lib/database.py
@@ -4,13 +4,14 @@ from typing import Any, Self
 from pprint import pprint
 from dataclasses import dataclass
 
-import jsonschema
 from jsonschema import validate, Draft202012Validator
+from haystack.dataclasses import Document
 
 class LocationType(Enum):
     LOCAL = "local"
     WEBSITE = "website"
 
+# Desc
 index_schema = {
     "type": "object",
     "patternProperties": {
@@ -40,14 +41,36 @@ index_schema = {
                 "additionalProperties": False
             }
         }
-    }
+    },
+    "additionalProperties": False
 }
 
 @dataclass(slots = True)
 class Link:
+    """
+    Represents a link between a collection of keywords and a file containing some text.
+    """
+
     keywords: list[str]
     location: str
     type: LocationType
+
+    def read_document(self) -> Document:
+        match self.type:
+            case LocationType.LOCAL:
+                with open(self.location, "r") as file:
+                    return Document(
+                        id = self.location,
+                        content = file.read()
+                    )
+            case LocationType.WEBSITE:
+                raise NotImplemented()
+            case _:
+                assert False, "Unreachable"
+
+    def match_keywords(self, text: str) -> bool:
+        return any(word in self.keywords for word in text.split())
+
 
 @dataclass(slots = True)
 class Subject:
@@ -87,8 +110,19 @@ class Database:
         validator = Draft202012Validator(index_schema)
         return validator.is_valid(data)
 
+    def fetch_relevant_documents(self, subject_name: str, text: str) -> list[Document]:
+        for subject in self.data:
+            if subject.name == subject_name:
+                result: list[Document] = []
+                for link in subject.links:
+                    if link.match_keywords(text):
+                        result.append(link.read_document())
+                return result
+        else:
+            raise ValueError(f"{subject_name} is not a valid subject name.")
 
 
 
 db = Database("../data/index.json")
 
+print(db.fetch_relevant_documents("Programmering 2", "Tell me how to make a class in java."))
